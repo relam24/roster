a15bf4205c338af8b3d71cf6cd3ba17c7cf21779
Rachel Elam <relam24@gmail.com>
2018-04-25 11:07:53 -0400

    fixed tabs

--- public/js/PlayersList.js
+++ public/js/PlayersList.js
@@ -1,27 +1,33 @@
 class PlayersList extends React.Component {
-  render (){
+	render () {
       // console.log(this.props.players);
-    return (
-      <table>
-        <tbody>
-        {this.props.players.map((player, index)=>{
-          return <tr>
-            <td onClick={()=> {this.props.toggleState('playersListIsVisible', 'playerIsVisible'); this.props.getPlayer(player)}}>
-              <h2>{player.player_name}</h2>
-            </td>
-            <td className='player'>
-              <h3> {player.status} </h3>
-            </td>
-            <td onClick={()=> {this.props.toggleState('playersListIsVisible', 'playerIsVisible'); this.props.getPlayer(player)}}>
-                <button className='button is-warning is-small'>Edit</button>
-            </td>
-            <td onClick={()=> {this.props.deletePlayer(player, index)}}>
-                <button className='button is-danger is-small'>Delete</button>
-            </td>
-          </tr>
-          })}
-        </tbody>
-      </table>
-    )
-  }
+		return (
+			<table>
+				<tbody>
+					{this.props.players.map((player, index) => {
+						return <tr>
+							<td onClick={() => {
+								this.props.toggleState('playersListIsVisible', 'playerIsVisible'); this.props.getPlayer(player);
+							}}>
+								<h2>{player.player_name}</h2>
+							</td>
+							<td className="player">
+								<h3> {player.status} </h3>
+							</td>
+							<td onClick={() => {
+								this.props.toggleState('playersListIsVisible', 'playerIsVisible'); this.props.getPlayer(player);
+							}}>
+								<button className="button is-warning is-small">Edit</button>
+							</td>
+							<td onClick={() => {
+								this.props.deletePlayer(player, index);
+							}}>
+								<button className="button is-danger is-small">Delete</button>
+							</td>
+						</tr>;
+					})}
+				</tbody>
+			</table>
+		);
+	}
 }
