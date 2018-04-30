07d1ae6a20a9c49240a65bcf586187081139de89
Tate Wilson <tatewilson4@gmail.com>
2018-04-30 14:16:07 -0600

    still trying to deploy onheroku

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,8 @@ class Team
     attr_reader :id, :team_name, :roster
 
 
-    if(ENV['https://heroku-roster.herokuapp.com/'])
-       uri = URI.parse(ENV['https://heroku-roster.herokuapp.com/'])
+    if(ENV['DATABASE_URL'])
+       uri = URI.parse(ENV['DATABASE_URL'])
        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
    else
        DB = PG.connect(host: "localhost", port: 5432, dbname: 'roster')
